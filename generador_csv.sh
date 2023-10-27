28f85246b0edcca72317cddeff43d8b2db25f7b8
CLozano03 <cesar.lozano.argueso@gmail.com>
2023-10-27 23:21:18 +0200

    Generacion .csv completada

--- generador_csv.sh
+++ generador_csv.sh
@@ -25,13 +25,17 @@ i=0
 echo "Num,Apellidos_Nombre,ID_FIDE,TITULO,STANDARD,RAPID,BLITZ" > ratings.csv
 
 function traducir_titulo(){
-    titulo=$1
+    titulo_f=$1
 
-    case "$titulo" in
-        "Grandmaster")      echo "GM";;
-        "International Master")     echo "IM";;
-        "Fide Master")   echo "FM";;
-        *) echo "";;
+    case "$titulo_f" in
+        "GRANDMASTER")                  echo "GM" ;;
+        "INTERNATIONALMASTER")          echo "IM" ;;
+        "FIDEMASTER")                   echo "FM" ;;
+        "CANDIDATEMASTER")              echo "CM" ;;
+        "WOMANGRANDMASTER")             echo "WGM" ;;
+        "WOMANINTERNATIONALMASTER")     echo "WIM" ;;
+        "WOMANCANDIDATEMASTER")         echo "WCM" ;;
+        "NONE")                         echo "";;
     esac
 }
 
@@ -44,12 +48,11 @@ while IFS= read -r id_FIDE; do
     curl -s $URL > $archivo_html
 
     #Obtencion de los datos
-    titulo=$(awk -v RS="</div>" 'RT{gsub(/.*FIDE title:<\/div><div class="profile-top-info__block__row__data>/, "", RT); print RT}' "$archivo_html")
-
-    #titulo=$(traducir_titulo $titulo)
-    rating_std=$(cat $archivo_html | grep -A1 '<span class="profile-top-rating-dataDesc">std</span>' | tail -n 1 | awk -F'<' '{print $1}' | tr -d '[:space:]')
-    rating_rapid=$(cat $archivo_html | grep -A1 '<span class="profile-top-rating-dataDesc">rapid</span>' | tail -n 1 | awk -F'<' '{print $1}' | tr -d '[:space:]')
-    rating_blitz=$(cat $archivo_html | grep -A1 '<span class="profile-top-rating-dataDesc">blitz</span>' | tail -n 1 | awk -F'<' '{print $1}' | tr -d '[:space:]')
+    titulo=$( cat $archivo_html | grep -A1 '<div class="profile-top-info__block__row__header">FIDE title:</div>' | awk -F'>|<' '{print $3}' | sed 's/FIDE title://g' | tr -d '\n' | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
+    titulo=$(traducir_titulo $titulo)
+    rating_std=$(cat $archivo_html | grep -A1 '<span class="profile-top-rating-dataDesc">std</span>' | tail -n 1 | awk -F'<' '{print $1}' | tr -d '[:space:]' | sed 's/Notrated/0/')
+    rating_rapid=$(cat $archivo_html | grep -A1 '<span class="profile-top-rating-dataDesc">rapid</span>' | tail -n 1 | awk -F'<' '{print $1}' | tr -d '[:space:]' | sed 's/Notrated/0/')
+    rating_blitz=$(cat $archivo_html | grep -A1 '<span class="profile-top-rating-dataDesc">blitz</span>' | tail -n 1 | awk -F'<' '{print $1}' | tr -d '[:space:]' | sed 's/Notrated/0/')
     apellidos_nombre=$(cat $archivo_html | grep '<div class="col-lg-8 profile-top-title">' | sed -n 's/.*>\(.*\)<\/div>/\U\1/p' | tr -d '\n' | tr -d ',' | sed 's/ *$//')
     
     #Escritura en el archivo CSV
